b03e070fcef54195368ce602879d72ea2aad7efe
Harmanpreet Kaur <Harmanpreet_Kaur@jabil.com>
2022-07-07 09:36:32 -0600

    initial commit [setup the console project]

---
+++ C-SharpBasics/C-SharpBasics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_SharpBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
