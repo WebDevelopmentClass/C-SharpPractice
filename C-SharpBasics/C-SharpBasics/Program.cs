f1f8e5ffc7c5f67852f02309694060c0f4fc4b22
Harmanpreet Kaur <Harmanpreet_Kaur@jabil.com>
2022-07-07 10:06:46 -0600

    practising user input

--- C-SharpBasics/C-SharpBasics/Program.cs
+++ C-SharpBasics/C-SharpBasics/Program.cs
@@ -8,12 +8,22 @@ namespace C_SharpBasics
         {
             // int , string, float, double , char, longint
 
-            var fName = "John";
-            var lName = "Miller";
+            //var fName = "John";
+            //var lName = "Miller";
 
-            var fullName = fName + lName;
+            //var fullName = fName + lName;
+
+            //Console.WriteLine(fullName);
+
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
            
-            Console.WriteLine(fullName);
+
+            Console.WriteLine($"Age of {name} is {age}");
 
         }
     }
