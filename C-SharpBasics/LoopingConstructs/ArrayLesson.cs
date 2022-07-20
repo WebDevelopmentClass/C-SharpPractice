38c1e756660a17128f4fbee043912f1a57e9dab5
Harmanpreet Kaur <Harmanpreet_Kaur@jabil.com>
2022-07-20 13:00:38 -0600

    feat: lopoing construct and array basics theory

---
+++ C-SharpBasics/LoopingConstructs/ArrayLesson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoopingConstructs
+{
+    internal class ArrayLesson
+    {
+        public void ArrayProg()
+        {
+            int[] rollNumbers = { 1,3,6,7,8,9};
+        }
+    }
+}
---
+++ C-SharpBasics/LoopingConstructs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoopingConstructs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
