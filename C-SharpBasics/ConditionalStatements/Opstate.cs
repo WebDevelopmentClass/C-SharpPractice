bb906353b22e6d1f025c6fde33c16c4c7b680143
Harmanpreet Kaur <Harmanpreet_Kaur@jabil.com>
2022-07-20 09:59:58 -0600

    Feat: ifelse and Ternary operator HW

---
+++ C-SharpBasics/ConditionalStatements/Opstate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConditionalStatements
+{
+    internal class Opstate
+    {
+        public bool ConditionalSate( int a)
+        {
+
+            if (a == 'a' || a == 'A' || a == 'e' || a =='E' || a == 'i' || a == 'I' || a == 'o' || a == 'O' || a == 'u' || a == 'U')
+                return true;
+            else 
+                return false;
+        }
+    }
+}
---
+++ C-SharpBasics/ConditionalStatements/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConditionalStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Opstate opstate = new Opstate();
+            Console.WriteLine("Enter any Character:");
+            Char a = char.Parse(Console.ReadLine());
+
+            var Checkchar = opstate.ConditionalSate(a);
+
+            string message = Checkchar ? "Entered Character is Vowel" : "Entered Character is Consonant";
+
+            Console.WriteLine(message);
+        }
+    }
+}
