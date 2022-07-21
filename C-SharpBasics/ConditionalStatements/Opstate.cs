7aa3a7964651a065e91558dd77a2fc987ccf2a0a
Harmanpreet Kaur <Harmanpreet_Kaur@jabil.com>
2022-07-20 19:48:50 -0600

    Largest of Three Numbers

--- C-SharpBasics/ConditionalStatements/Opstate.cs
+++ C-SharpBasics/ConditionalStatements/Opstate.cs
@@ -14,5 +14,15 @@ namespace ConditionalStatements
             else 
                 return false;
         }
+        public int MaxThreeNumbers(int a, int b, int c)
+        {
+            if (a>b && a>c)
+                return a;
+            else if (b>c && b>a)
+                return b;
+            else
+                return c;
+                
+        }
     }
 }
--- C-SharpBasics/ConditionalStatements/Program.cs
+++ C-SharpBasics/ConditionalStatements/Program.cs
@@ -7,14 +7,24 @@ namespace ConditionalStatements
         static void Main(string[] args)
         {
             Opstate opstate = new Opstate();
-            Console.WriteLine("Enter any Character:");
-            Char a = char.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter any Character:");
+            //Char a = char.Parse(Console.ReadLine());
 
-            var Checkchar = opstate.ConditionalState(a);
+            //var Checkchar = opstate.ConditionalState(a);
 
-            string message = Checkchar ? "Entered Character is Vowel" : "Entered Character is Consonant";
+            //string message = Checkchar ? "Entered Character is Vowel" : "Entered Character is Consonant";
 
-            Console.WriteLine(message);
+            //Console.WriteLine(message);
+
+
+            Console.WriteLine("Enter any three number:");
+            int a = int.Parse(Console.ReadLine());
+            int b =int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int message = opstate.MaxThreeNumbers(a, b, c); 
+            Console.WriteLine("Largest number is {0}",message);
+            
         }
     }
 }
