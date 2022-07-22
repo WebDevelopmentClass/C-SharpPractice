5aadda12f79ca0f673e457a5bf4214e5db36f968
Harmanpreet Kaur <Harmanpreet_Kaur@jabil.com>
2022-07-22 12:13:39 -0600

    Sum of first 10 numbers

--- C-SharpBasics/ConditionalStatements/Opstate.cs
+++ C-SharpBasics/ConditionalStatements/Opstate.cs
@@ -6,37 +6,41 @@ namespace ConditionalStatements
 {
     internal class Opstate
     {
-    //    public bool ConditionalState(char a)
-    //    {
+        //    public bool ConditionalState(char a)
+        //    {
 
-    //        if (a == 'a' || a == 'A' || a == 'e' || a =='E' || a == 'i' || a == 'I' || a == 'o' || a == 'O' || a == 'u' || a == 'U')
-    //            return true;
-    //        else 
-    //            return false;
-    //    }
-    //    public int MaxThreeNumbers(int a, int b, int c)
-    //    {
-    //        if (a>b && a>c)
-    //            return a;
-    //        else if (b>c && b>a)
-    //            return b;
-    //        else
-    //            return c;
-                
-    //    }
+        //        if (a == 'a' || a == 'A' || a == 'e' || a =='E' || a == 'i' || a == 'I' || a == 'o' || a == 'O' || a == 'u' || a == 'U')
+        //            return true;
+        //        else 
+        //            return false;
+        //    }
+        //    public int MaxThreeNumbers(int a, int b, int c)
+        //    {
+        //        if (a>b && a>c)
+        //            return a;
+        //        else if (b>c && b>a)
+        //            return b;
+        //        else
+        //            return c;
+
+        //    }
 
         public int SumofNaturalNumbers()
         {
-            int i=1;
-            int a=0;
+            int i = 1;
+            int a = 0;
+            int[] userArray = { };
+            userArray[0] = 1;
+            userArray[1] = 23;
+
             for (i = 1; i <= 10; i++)
             {
-                a = a+ i;
-               
+                a = a + i;
+
             }
             return a;
-
-
         }
+           
+           
     }
 }
