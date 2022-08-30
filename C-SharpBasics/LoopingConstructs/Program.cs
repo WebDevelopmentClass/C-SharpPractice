766f7022ca15159ac62296848402055d94acd3d8
Pranav kumar <sanupranav@outlook.com>
2022-08-30 22:00:34 +0530

    feat: Lists and Models

--- C-SharpBasics/LoopingConstructs/Program.cs
+++ C-SharpBasics/LoopingConstructs/Program.cs
@@ -7,15 +7,49 @@ namespace LoopingConstructs
     {
         static void Main(string[] args)
         {
-            ReadProperties();
+
+
+            List<Subject> subjects = new List<Subject>
+            {
+                 new Subject{ SubjectCode = 101,SubjectName= "SCIENCE", SubjectType="SCI-FI"},
+                 new Subject{ SubjectCode = 102,SubjectName= "HINDI", SubjectType="LIT"},
+                 new Subject{ SubjectCode = 103,SubjectName= "ENGLISH", SubjectType="ENG"},
+                 new Subject{ SubjectCode = 104,SubjectName= "LITERATURE", SubjectType="LIT"},
+            };
+
+            List<Student> students = new List<Student> { new Student { Name = } }
+
+            List<City> cities = new List<City>();
+
+            Student student = new Student();
+            student.Name = "Harman Kaur";
+            student.Age = 25;
+            student.RollNumber = 1;
+          //  student.Standard = "Btech";
+            student.Subjects = subjects;
+
+
+
+            Console.WriteLine($"Name Of Student is: {student.Name}");
+            Console.WriteLine($"Age Of Student is: {student.Age}");
+            Console.WriteLine($"Roll Of Student is: {student.RollNumber}");
+            //Console.WriteLine($"Standard Of Student is: {student.Standard}");
+            foreach (var item in student.Subjects)
+            {
+                Console.WriteLine($"\t\t Subject Code is {item.SubjectCode}");
+                Console.WriteLine($"\t\t Subject Code is {item.SubjectName}");
+                Console.WriteLine($"\t\t Subject Code is {item.SubjectType}");
+                Console.WriteLine($"\n");
+            }
+            //ReadProperties();
             //Snumber();
 
-            Console.WriteLine("Learning Array");
+            //Console.WriteLine("Learning Array");
             //ArrayLesson array = new ArrayLesson();
             //array.ArrayProg();
 
-            ForLoop loop = new ForLoop();
-            loop.ForeachLoop();
+            //ForLoop loop = new ForLoop();
+            //loop.ForeachLoop();
         }
         public static void Snumber()
         {
@@ -39,10 +73,10 @@ namespace LoopingConstructs
 
         public static void ReadProperties()
         {
-            Student student= new Student();
+            Student student = new Student();
 
             Console.WriteLine("Enter Student name: ");
-            student.Name = Console.ReadLine();  
+            student.Name = Console.ReadLine();
 
             Console.WriteLine(student.Name);
 
@@ -50,8 +84,8 @@ namespace LoopingConstructs
 
             List<string> listOfString;
             // changes 
-     
-           
+
+
         }
 
 
--- C-SharpBasics/LoopingConstructs/Properties.cs
+++ C-SharpBasics/LoopingConstructs/Properties.cs
@@ -10,12 +10,32 @@ namespace LoopingConstructs
         public int Age { get; set; }
     }
 
+    // model 
     internal class Student
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
         public int RollNumber { get; set; }
-        public string Standard { get; set; }
+        public List<Subject> Subjects { get; set; }
+    }
+
+    internal class Subject
+    {
+        public int SubjectCode { get; set; }
+        public string SubjectName { get; set; }
+        public string SubjectType { get; set; }
+    }
+
+    internal class Standard
+    {
+        public string Standards { get; set; }
+        public List<Student> Students { get; set; }
+    }
+    internal class City
+    {
+        public string CityName { get; set; }
+        public string PinCode { get; set; }
     }
 }
+
